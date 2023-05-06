38a84712545c907391fd400695bdc0063f838d4e
mansour2391 <cheikhmansour.sow@unchk.edu.sn>
2023-05-06 11:50:32 +0000

    premier partie du projet deja fait

--- somme.py
+++ somme.py
@@ -1,1 +1,9 @@
-nbre
+# Trouver la somme de deux nombres entiers
+nbr1 = input('Entrez le premier nombre: ')
+nbr2 = input('Entrez le deuxième nombre: ')
+
+# Additionner les deux nombres 
+s = int(nbr1) + int(nbr2)
+
+# Afficher la somme
+print('La somme de {0} et  {1} est {2}'.format(nbr1, nbr2, s))
